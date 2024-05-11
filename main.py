1a44bfdca8d1169b82e53bbba618b19607c96bf1
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2024-05-11 17:11:13 +0200

    const variables refactor

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from api_key import key
 
 app = FastAPI()
 
-base_url = "https://api.clashofclans.com/v1"
-players_endpoint = "players"
-leagues_endpoint = "leagues/"
-clans_endpoint = "clans"
-location_endpoint = "locations"
+BASE_URL = "https://api.clashofclans.com/v1"
+PLAYERS_ENDPOINT = "players"
+LEAGUES_ENDPOINT = "leagues/"
+CLANS_ENDPOINT = "clans"
+LOCATION_ENDPOINT = "locations"
 final_url = None
 
 headers = {
@@ -18,21 +18,21 @@ headers = {
 
 @app.get("/coc-api/players/{player_tag}")
 def get_player(player_tag: str):
-    final_url = f'{base_url}/{players_endpoint}/%23{player_tag}'
+    final_url = f'{BASE_URL}/{PLAYERS_ENDPOINT}/%23{player_tag}'
     response = requests.get(final_url, headers=headers)
     data = response.json()
     return data
 
 @app.get("/coc-api/clans/{clan_tag}")
 def get_player(clan_tag: str):
-    final_url = f'{base_url}/clans/%23{clan_tag}'
+    final_url = f'{BASE_URL}/clans/%23{clan_tag}'
     response = requests.get(final_url, headers=headers)
     data = response.json()
     return data
 
 @app.get("/coc-api/locations/{location_id}/rankings/players")
 def get_player(location_id: str):
-    final_url = f'{base_url}/{location_endpoint}/{location_id}/rankings/players'
+    final_url = f'{BASE_URL}/{LOCATION_ENDPOINT}/{location_id}/rankings/players'
     response = requests.get(final_url, headers=headers)
     data = response.json()
     return data
