97331867afdd2b19909ac37a66178cfc928b0f43
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2024-05-26 04:21:07 +0200

    fixed clans badgeUrls from rankings/players endpoint + new customs endpoints + refactor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import requests
 from api_key import key
 from fastapi.middleware.cors import CORSMiddleware
+import logging
 
 app = FastAPI()
 
@@ -33,6 +34,8 @@ def make_request(url: str):
     response = requests.get(url, headers=HEADERS)
     return response.json()
 
+# OFFICIAL API ENDPOINTS DUPLICATES
+
 @app.get("/")
 def get_root():
     return "<p>It works</p>"
@@ -53,10 +56,36 @@ def get_locations():
     response = make_request(final_url)
     locations = response['items']
     return locations
-        
+
+# CUSTOM API ENDPOINTS
+
 @app.get("/coc-api/locations/{location_id}/rankings/players")
 def get_rankings_players_by_location(location_id: str):
-    final_url = f'{BASE_URL}/{LOCATION_ENDPOINT}/{location_id}/{RANKINGS_ENDPOINT}/{PLAYERS_ENDPOINT}'
+    final_url = f'{BASE_URL}/{LOCATION_ENDPOINT}/{location_id}/{RANKINGS_ENDPOINT}/{PLAYERS_ENDPOINT}?limit=10'
     response = make_request(final_url)
     players = response['items']
+    for player in players:
+        player_clan = get_player_clan(player['tag'][1:])
+        if player_clan is not None:
+            try:
+                player['clan']['badgeUrls'] = player_clan['badgeUrls']
+            except:
+                pass
     return players
+
+@app.get("/coc-api/players/{player_tag}/clan")
+def get_player_clan(player_tag: str):
+    final_url = f'{BASE_URL}/{PLAYERS_ENDPOINT}/%23{player_tag}'
+    player = make_request(final_url)
+    if player.get('clan') is None:
+        return None
+    return player['clan']
+
+@app.get("/coc-api/countries")
+def get_countries():
+    final_url = f'{BASE_URL}/{LOCATION_ENDPOINT}'
+    response = make_request(final_url)
+    locations = response['items']
+    countries = [location for location in locations if location['isCountry']]
+    return countries
+        
